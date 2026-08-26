3c646bf121970a8ffae855852d09a6a7be25d235
sriharshavenkat129 <sriharshavenkat123@gmail.com>
2026-08-26 21:14:14 +0530

    updated cart controllers

--- backend/src/controllers/cartControllers.js
+++ backend/src/controllers/cartControllers.js
@@ -1,115 +1,120 @@
 const { error } = require("node:console")
-const pool=require("../config/db")
+const pool = require("../config/db")
 
-const addProductIntoCart=async (req,res,next)=>{
-    const {product_id}=req.body
-    if(!product_id)
-        return next({"status":400,"msg":"product_id required!"})
-    try{
-        const result=await pool.query("select * from carts where user_id=$1 and product_id=$2",[req.user.user_id,product_id])
-        if(result.rows.length==1)
-            return next({'status':409,"msg":'product already in cart!'})
-        await pool.query("insert into carts (product_id,user_id) values($1,$2)",[product_id,req.user.user_id])
-        res.status(200).json({"msg":"product added into cart"})
+const addProductIntoCart = async (req, res, next) => {
+    const { product_id } = req.body
+    if (!product_id)
+        return next({ "status": 400, "msg": "product_id required!" })
+    try {
+        const result = await pool.query("select * from carts where user_id=$1 and product_id=$2", [req.user.user_id, product_id])
+        if (result.rows.length == 1)
+            return next({ 'status': 409, "msg": 'product already in cart!' })
+        await pool.query("insert into carts (product_id,user_id) values($1,$2)", [product_id, req.user.user_id])
+        res.status(200).json({ "msg": "product added into cart" })
     }
-    catch(error){
-        next({"status":500,"msg":"adding product into cart failed!"})
+    catch (error) {
+        next({ "status": 500, "msg": "adding product into cart failed!" })
     }
 }
 
-const getAllCartedProducts=async (req,res,next)=>{
-    try{
-        const result=await pool.query("select c.cart_id,p.product_id,p.product_name,p.product_image,p.price,p.stock,p.is_available\
-            from carts c left join products p on c.product_id=p.product_id where c.user_id=$1",[req.user.user_id])
-        if(result.rows.length==0)
-            return next({"status":404,"msg":"car is empty right now!"})
-        res.status(200).json({"msg":"cart fetched successfully","data":result.rows})
+const getAllCartedProducts = async (req, res, next) => {
+    try {
+        const result = await pool.query("select c.cart_id,p.product_id,p.product_name,p.product_description,p.product_image,p.price,p.stock,p.is_available\
+            from carts c left join products p on c.product_id=p.product_id where c.user_id=$1", [req.user.user_id])
+        if (result.rows.length == 0)
+            return next({ "status": 404, "msg": "car is empty right now!" })
+        res.status(200).json({ "msg": "cart fetched successfully", "data": result.rows })
     }
-    catch(error){
-        next({"status":500,"msg":"something went wrong!"})
-    }
-}
-
-const removeCartedProduct=async (req,res,next)=>{
-    const cart_id=req.params.cart_id
-    if(!cart_id)
-        return next({"status":400,"msg":"cart_id required!"})
-    try{
-        await pool.query("delete from carts where cart_id=$1",[cart_id])
-        res.status(200).json({"msg":'product removed form the cart'})
-    }
-    catch(error){
-        next({"status":500,"msg":"failed to remove the product"})
+    catch (error) {
+        next({ "status": 500, "msg": "something went wrong!" })
     }
 }
 
-const orderAllFromCart=async (req,res,next)=>{
-    const {cartedProducts,payment_type,address_id}=req.body
-    const status=payment_type=="upi"?"completed":"pending"
-    if(cartedProducts.length==0)
-        return next({"status":400,"msg":"no products in the cart to process order"})
+const removeCartedProduct = async (req, res, next) => {
+    const cart_id = req.params.cart_id
+    if (!cart_id)
+        return next({ "status": 400, "msg": "cart_id required!" })
+    try {
+        await pool.query("delete from carts where cart_id=$1", [cart_id])
+        res.status(200).json({ "msg": 'product removed form the cart' })
+    }
+    catch (error) {
+        next({ "status": 500, "msg": "failed to remove the product" })
+    }
+}
+
+const orderAllFromCart = async (req, res, next) => {
+    const { cartedProducts, payment_type, address_id } = req.body
+    if(!payment_type || !address_id)return res.status(400).json({"msg":"address and payment_type required"})
+    const status = payment_type == "upi" ? "completed" : "pending"
+    if (cartedProducts.length == 0)
+        return next({ "status": 400, "msg": "no products in the cart to process order" })
     let con;
-    try{
-        con=await pool.connect()
+    try {
+        let orderedProducts = []
+        con = await pool.connect()
         con.query('begin')
-        for(const cartedProduct of cartedProducts){
-            try{
-                const {cart_id,product_id,quantity}=cartedProduct
-                if(!cart_id || !product_id ||!quantity){
+        for (const cartedProduct of cartedProducts) {
+            try {
+                const { cart_id, product_id, quantity } = cartedProduct
+                if (!cart_id || !product_id || !quantity) {
                     con.query('rollback')
-                    return next({"status":400,"msg":"product_id ,cart_id and quantity are required!"})
+                    return next({ "status": 400, "msg": "product_id ,cart_id and quantity are required!" })
                 }
-                if(quantity<=0){
+                if (quantity <= 0) {
                     await con.query('rollback')
-                    return next({"status":400,"msg":"qunatity alteast be one"})
+                    return next({ "status": 400, "msg": "qunatity alteast be one" })
                 }
-                const cart=await con.query("select * from carts where cart_id=$1",[cart_id])
-                if(cart.rows.length==0){
+                const cart = await con.query("select * from carts where cart_id=$1", [cart_id])
+                if (cart.rows.length == 0) {
                     await con.query('rollback')
-                    return next({"status":404,"msg":"failed to place the order"})
+                    return next({ "status": 404, "msg": "failed to place the order" })
                 }
-                const product_details=await con.query("select stock,price,is_available from products where product_id=$1",[product_id])
-                if(product_details.rows.length==0){
+                const product_details = await con.query("select stock,price,is_available from products where product_id=$1", [product_id])
+                if (product_details.rows.length == 0) {
                     await con.query('rollback')
-                    return next({"status":404,"msg":"product not found!"})
+                    return next({ "status": 404, "msg": "product not found!" })
                 }
-                const product=product_details.rows[0]
-                if(quantity>product.stock || product.is_available==false){
+                const product = product_details.rows[0]
+                if (quantity > product.stock || product.is_available == false) {
                     await con.query('rollback')
-                    return next({"status":400,"msg":"product is out of stock for your order!"})
+                    return next({ "status": 400, "msg": "product is out of stock for your order!" })
                 }
                 await con.query("update products set stock=$1 where product_id=$2",
-                    [product.stock-quantity,product_id]
+                    [product.stock - quantity, product_id]
                 )
-                const transaction_details=await con.query("insert into transactions (payment_type,total_amount,payment_status)\
-                    values($1,$2,$3) returning transaction_id",[payment_type,product_details.rows[0].price*quantity,status])
-                    if(transaction_details.rows.length==0){
-                        await con.query('rollback')
-                        return next({"status":500,"msg":"order not placed!"})
-                    }
-                    await con.query("insert into orders (user_id,product_id,address_id,quantity,transaction_id,unit_price)\
-                        values($1,$2,$3,$4,$5,$6)",[req.user.user_id,product_id,address_id,quantity,
-                            transaction_details.rows[0].transaction_id,product.price])
-                        }
-                catch(error){
-                    throw new Error("unable to process the order")
+                const transaction_details = await con.query("insert into transactions (payment_type,total_amount,payment_status)\
+                    values($1,$2,$3) returning transaction_id", [payment_type, product_details.rows[0].price * quantity, status])
+                if (transaction_details.rows.length == 0) {
+                    await con.query('rollback')
+                    return next({ "status": 500, "msg": "order not placed!" })
                 }
-                } 
-                await con.query('delete from carts where user_id=$1',[req.user.user_id])
-                await con.query('commit')
-                res.status(201).json({"msg":"order placed successfully"})
+                await con.query("insert into orders (user_id,product_id,address_id,quantity,transaction_id,unit_price)\
+                        values($1,$2,$3,$4,$5,$6)", [req.user.user_id, product_id, address_id, quantity,
+                transaction_details.rows[0].transaction_id, product.price])
+
+                orderedProducts.push(product_id)
+            }
+            catch (error) {
+                throw new Error("unable to process the order")
+            }
+        }
+        const ordered_product_ids = `${orderedProducts.join(",")}`
+        await con.query('delete from carts where user_id=$1 and product_id in ($2)', [req.user.user_id, ordered_product_ids])
+        await con.query('commit')
+        res.status(201).json({ "msg": "order placed successfully" })
     }
-    catch(error){
-        if(con)
+    catch (error) {
+        if (con)
             await con.query('rollback')
-        next({"msg":500,"msg":"unable to place the order"})
+        next({ "msg": 500, "msg": "unable to place the order" })
     }
-    finally{
-        if(con)
+    finally {
+        if (con)
             await con.release()
     }
 }
 
 
 
-module.exports={addProductIntoCart,getAllCartedProducts,removeCartedProduct,orderAllFromCart}
+module.exports = { addProductIntoCart, getAllCartedProducts, removeCartedProduct, orderAllFromCart }
